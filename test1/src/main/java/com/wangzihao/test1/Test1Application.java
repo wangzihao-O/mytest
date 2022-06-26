a3ee55011326b94a98c3d268ad5fd1dc17ed8eac
wangzihao <1973000476@qq.com>
2022-06-26 22:15:03 +0800

    main 更新 test1

--- test1/src/main/java/com/wangzihao/test1/Test1Application.java
+++ test1/src/main/java/com/wangzihao/test1/Test1Application.java
@@ -13,7 +13,7 @@ public class Test1Application {
 
     public static void test1(){
         System.out.println("hello world ");
-        System.out.println("main");
+        System.out.println("i am main");
     }
 
 }

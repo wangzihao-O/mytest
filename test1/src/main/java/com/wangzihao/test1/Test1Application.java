9f764100781f2707ce9b7d9841fba845885fbd29
wangzihao <1973000476@qq.com>
2022-06-26 23:23:51 +0800

    功能3 function3

--- test1/src/main/java/com/wangzihao/test1/Test1Application.java
+++ test1/src/main/java/com/wangzihao/test1/Test1Application.java
@@ -33,5 +33,8 @@ public class Test1Application {
         System.out.println("function2 have 1");
         System.out.println("function2 have 2");
     }
+    public static void function3(){
+        System.out.println("i am function3 !");
+    }
 
 }
